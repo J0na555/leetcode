da4c3b42463cafe1474dfc49cae240e7b4f00ad0
J0na555 <ethiopediaa@gmail.com>
2025-10-06 21:24:25 +0300

    Runtime 2055ms Beats27.52%

---
+++ 1. Two Sum/TwoSum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return []
+
+
+# the outer loop iterates through each elements except for the last one
+# teh inner loop iterates through each elements after i
