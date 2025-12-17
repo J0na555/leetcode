b6400b07fd74837940eda8658746f92c5d7b4dc2
J0na555 <ethiopediaa@gmail.com>
2025-12-17 20:30:11 +0300

    Runtime 2ms Beats 96.79%

---
+++ 605. Can Place Flowers/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        
+        # dummy boundary so that we can get the edge cases when the first and second elements are 0
+        flowerbed = [0] + flowerbed + [0]
+
+        for i in range(1, len(flowerbed)-1):
+
+            if flowerbed[i - 1] == 0 and  flowerbed[i] == 0 and flowerbed[i+1] == 0:
+                flowerbed[i] = 1
+                n -= 1
+
+                if n < 1:
+                    return True
+            
+        return n <= 0
